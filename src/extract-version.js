edc5aa9dd3438692240f9df69b3b8ef1966186c6
Saionaro <shibakow@gmail.com>
2020-03-05 18:04:54 +0300

    Remove debugs

--- src/extract-version.js
+++ src/extract-version.js
@@ -6,16 +6,8 @@ const workspace = process.env.GITHUB_WORKSPACE;
 async function run() {
   try {
     const packagePath = path.join(workspace, "package.json");
-
-    core.debug(packagePath);
-
     const pkg = require(packagePath);
-
-    core.debug(pkg);
-
     const version = pkg.version.toString();
-
-    core.debug(version);
 
     core.setOutput("version", version);
   } catch (error) {
