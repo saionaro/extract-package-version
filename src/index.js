70c3909a71db76d40d4b35cc6cbf0bc6f36cfd23
Artem Shibakov <shibakow@gmail.com>
2026-04-04 23:47:01 +0400

    cleanup (#26)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const run = require("./extract-version");
+import path from "node:path";
+import { readFile } from "node:fs/promises";
+import { getInput, setOutput, setFailed } from "@actions/core";
 
-if (require.main === module) {
-  run();
+const { GITHUB_WORKSPACE } = process.env;
+
+try {
+  let dir = getInput("path") || GITHUB_WORKSPACE;
+  dir = path.resolve(dir);
+
+  const packagePath = path.join(dir, "package.json");
+  const rawContent = await readFile(packagePath, "utf-8");
+  const pkg = JSON.parse(rawContent);
+  const version = pkg.version.toString();
+
+  setOutput("version", version);
+} catch (error) {
+  setFailed(error.message);
 }
